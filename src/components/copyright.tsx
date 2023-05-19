2177415bd4e19c130d93cb7cc88fadb9483d4c8a
elyon.ortiz <elyon.ortiz08@gamil.com>
2023-05-19 10:30:10 -0400

    lesson3

---
+++ src/components/copyright.tsx
@@ -0,0 +1,15 @@
+export const Copyright = () => {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 no NLW da{' '}
+      <a
+        href="https://rocketseat.com.br"
+        className="underline hover:text-gray-100"
+        target="-blank"
+        rel="noreferrer"
+      >
+        Rocketseat
+      </a>
+    </div>
+  )
+}
---
+++ src/components/herosection.tsx
@@ -0,0 +1,23 @@
+import nlwLogo from '../assets/nlw-spacingtime-logo.svg'
+import Image from 'next/image'
+
+export const HeroSection = () => {
+  return (
+    <div className="space-y-5">
+      <Image src={nlwLogo} alt="nlwLogo" />
+      <div>
+        <h1 className="space-y-5">Sua cápsula do tempo</h1>
+        <p className="text-lg leading-relaxed">
+          Colecione momentos marcantes da sua jornada e compartilhe (se quiser)
+          com o mundo!
+        </p>
+      </div>
+      <a
+        className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-700"
+        href=""
+      >
+        CADASTRAR LEMBRANÇA
+      </a>
+    </div>
+  )
+}
